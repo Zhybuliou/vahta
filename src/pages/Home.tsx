672580acd0facf1fa4c920e29fcd58d7a4339851
Vitali Zhybuliou <zhybuliou.v.v@gmail.com>
2023-06-09 15:37:43 +0300

    feat: add bg image for home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
+    <>
+    <div className="home-top"></div>
     <div className="wrapper-content">
       <h2>Hello world!!!</h2>
     </div>
+    </>
   );
 }
